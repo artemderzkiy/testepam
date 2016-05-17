6f958116aad885e696f9cd8cc8d6b8d3a3f72d32
artemderzkiy <amironich1993@gmail.com>
2016-05-17 23:12:11 +0400

    with modal and edit

--- src/cotroller.js
+++ src/cotroller.js
@@ -4,20 +4,25 @@ testApp.controller("itemCtrl", function($scope) {
     $scope.sortReverse  = false;  
     $scope.model = {
         items: [
-            {'name': "чайник", 'count': 4, 'price': "40"},
-            {'name': "ведро", 'count': 5, 'price': "50"},
-            {'name': "ваза", 'count': 6, 'price': "60"}
+            {'name': "чайник", 'count': 4, 'price': 40},
+            {'name': "ведро", 'count': 5, 'price': 50},
+            {'name': "ваза", 'count': 6, 'price': 60}
         ],
-        selected: {}
+        selected: {},
+        name : ""
+        
     };
 //добавление
-    $scope.addRow = function () {
-        $scope.model.items.push({'name': $scope.name, 'count':$scope.count, 'price': $scope.price});
-        $scope.name = '';
-        $scope.count = '';
-        $scope.price = '';
+ ///   $scope.addRow = function () {
+  //      $scope.model.items.push({'name': $scope.model.selected.name, 'count':$scope.model.selected.count, 'price': $scope.model.selected.price});
+  //      $scope.model.selected.name = '';
+  //      $scope.model.selected.count = '';
+  //      $scope.model.selected.price = '';
+    
+   
+    
 
-    };
+   // };
 //удаление
 $scope.remove = function (name) {
     var ans = confirm("Вы подтверждаете удаление?");
@@ -38,19 +43,32 @@ $scope.remove = function (name) {
 };
 //не воркает
     $scope.edit = function (item) {
+        $scope.model.name=item.name;
         $scope.model.selected = angular.copy(item);
     };
 //тоже почему то
     $scope.save = function (name) {
         console.log("Saving contact");
-        $scope.model.items[name] = angular.copy($scope.model.selected);
+        if (!name )
+        {
+            $scope.model.items.push(angular.copy($scope.model.selected));
+            $scope.reset();
+        }
+
+        var idx=$scope.model.items.findIndex(function (element,index,array) {
+            return element.name===name;
+
+        });
+        $scope.model.items[idx] = angular.copy($scope.model.selected);
+
+        console.log($scope.model.selected);
         $scope.reset();
     };
 
 //пока не ебу для чего
     $scope.reset = function () {
         $scope.model.selected = {};
-    };
+   };
 
 
 
