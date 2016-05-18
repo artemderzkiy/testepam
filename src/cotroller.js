ae5dc76a3c801e9bdd3e4ce09d2a3563d9959ae1
artemderzkiy <amironich1993@gmail.com>
2016-05-18 22:42:29 +0400

    best version

--- src/cotroller.js
+++ src/cotroller.js
@@ -4,51 +4,56 @@ testApp.controller("itemCtrl", function($scope) {
     $scope.sortReverse  = false;  
     $scope.model = {
         items: [
-            {'name': "чайник", 'count': 4, 'price': 40},
-            {'name': "ведро", 'count': 5, 'price': 50},
-            {'name': "ваза", 'count': 6, 'price': 60}
+            {'name': "chair", 'count': 4, 'price': 40},
+            {'name': "table", 'count': 5, 'price': 50},
+            {'name': "carpet", 'count': 6, 'price': 60},
+            {'name': "window", 'count': 2, 'price': 80},
+            {'name': "TV", 'count': 15, 'price': 400},
+            {'name': "wardrobe", 'count': 1, 'price': 133},
+            {'name': "pillow", 'count': 35, 'price': 5},
+            {'name': "blanket", 'count': 22, 'price': 8},
         ],
         selected: {},
         name : ""
         
     };
-//добавление
- ///   $scope.addRow = function () {
-  //      $scope.model.items.push({'name': $scope.model.selected.name, 'count':$scope.model.selected.count, 'price': $scope.model.selected.price});
-  //      $scope.model.selected.name = '';
-  //      $scope.model.selected.count = '';
-  //      $scope.model.selected.price = '';
-    
-   
-    
+    /*
+//adding non actual
+   $scope.addRow = function () {
+       $scope.model.items.push({'name': $scope.model.selected.name, 'count':$scope.model.selected.count, 'price': $scope.model.selected.price});
+       $scope.model.selected.name = '';
+       $scope.model.selected.count = '';
+       $scope.model.selected.price = '';
+      };
+   */
 
-   // };
-//удаление
+//delete
 $scope.remove = function (name) {
-    var ans = confirm("Вы подтверждаете удаление?");
-    if (ans==true) {
+    var ans = confirm("Are you sure?");
+   if (ans==true) {
         var comArr = eval($scope.model.items);
         for (var i = 0; i < comArr.length; i++) {
             if (comArr[i].name === name) {
-                index = i;
-                break;
-            }
+    var index = i;
+    break;
+}
         }
+    console.log(index);
         if (index === -1) {
             alert("something gone wrong");
         }
         $scope.model.items.splice(index, 1);
-    }
+   }
     
 };
-//не воркает
+//edit
     $scope.edit = function (item) {
         $scope.model.name=item.name;
         $scope.model.selected = angular.copy(item);
     };
-//тоже почему то
+//saving to model
     $scope.save = function (name) {
-        console.log("Saving contact");
+        console.log("Saving item");
         if (!name )
         {
             $scope.model.items.push(angular.copy($scope.model.selected));
@@ -60,19 +65,38 @@ $scope.remove = function (name) {
 
         });
         $scope.model.items[idx] = angular.copy($scope.model.selected);
+             $scope.reset();
 
-        console.log($scope.model.selected);
-        $scope.reset();
     };
 
-//пока не ебу для чего
+
     $scope.reset = function () {
         $scope.model.selected = {};
+        $scope.model.name="";
+        $('#myModal').modal('hide');
    };
 
 
+//filter for price to convert into money
+    testApp.filter('currency', function() {
+        return function(number, currencyCode) {
+            var currency = { $: "$" },
+                thousand, decimal, format;
+            if ($.inArray(currencyCode, ["$"]) >= 0)
+            {
+                thousand = ",";
+                decimal = ".";
+                format = "%s%v";
+            };
+            return accounting.formatMoney(number, currency[currencyCode], 2, thousand, decimal, format);
+        }
+        });
+
 
 });
+
+
+
 
 
 /**
